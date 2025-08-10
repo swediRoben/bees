0f964f0832bee6a2a2d4bd32d7cef47b2edf057b
msfaraja <msfaraja@asystresources.com>
2025-08-10 18:04:06 +0200

    mon projet

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bees/",
+  base: "/bees/", // le nom exact de ton repo GitHub
   build: {
     rollupOptions: {
-      input: '/index.html',
+      input: 'index.html', // chemin relatif, sans slash au début
     }
   },
 })
- 
