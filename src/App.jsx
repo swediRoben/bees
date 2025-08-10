3e38642d136f575cab00401ae5ed60aef9d7ab27
msfaraja <msfaraja@asystresources.com>
2025-08-10 18:48:42 +0200

    first page reals derniere test a cette seconde

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react' 
+ 
 import Welcome from './welcome/welcome' 
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     <Welcome/>
-    </>
-  )
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+ 
+function About() {
+  return <h2>À propos</h2>;
 }
 
-export default App
+function NotFound() {
+  return <h2>Page non trouvée</h2>;
+}
+
+function App() {
+  return (
+    <BrowserRouter basename="/bees">
+      <nav style={{ marginBottom: 20 }}>
+        <Link to="/">Accueil</Link> |{' '}
+        <Link to="/about">À propos</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={ <Welcome/>} />
+        <Route path="/about" element={<About />} />
+        {/* Route catch-all pour les urls inconnues */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
