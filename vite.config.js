aabccf4d395bfd0c51b37ea20cf260d0e13d32a3
msfaraja <msfaraja@asystresources.com>
2025-08-10 18:05:45 +0200

    first page reals

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bees/", // le nom exact de ton repo GitHub
-  build: {
-    rollupOptions: {
-      input: 'index.html', // chemin relatif, sans slash au début
-    }
-  },
-})
+  base: "/swediroben.github.io/", // ou ton repo, ex: "/bees/"
+});
