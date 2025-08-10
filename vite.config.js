029c27a6841ae3c39e4508fb86a184953ead8755
msfaraja <msfaraja@asystresources.com>
2025-08-10 17:59:30 +0200

    first page reals

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/bees/",
   plugins: [react()],
+  base: "/bees/",
   build: {
     rollupOptions: {
       input: '/index.html',
